9008bb4f5692873161984b9a8d393e49b8337a61
OsteroyergoyJA <oosteroyergoy@gmail.com>
2024-12-11 12:27:27 +0100

    Summeringskalkulator

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const submitButton = document.querySelector('#submit-button');
 
 submitButton.addEventListener('click', function () {
-  const number1 = document.querySelector('#number1').value; // Corrected to .value
-  const number2 = document.querySelector('#number2').value; // Corrected to .value
-
+  
+  const number1 = document.querySelector('#number1').value; 
+  const number2 = document.querySelector('#number2').value;
   const sum = parseFloat(number1) + parseFloat(number2);
 
   console.log(sum);
@@ -13,3 +13,6 @@ submitButton.addEventListener('click', function () {
   newElement.textContent = sum;
   parentElement.appendChild(newElement);
 });
+ 
+/* Trenger en forklaring på hvorfor number1 og number2 må være innenfor funksjonen, 
+trodde de ble definert utenfor også kunne funksjonen hentet de? */
